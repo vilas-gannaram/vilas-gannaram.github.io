f9b60b3a2d929715652b4f81094f9f1f540db652
vilas <vilasgannaram@gmail.com>
2025-08-04 17:48:28 +0530

    add lenis scroll

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import Lenis from 'lenis';
 	import Header from '../lib/components/header.svelte';
 
 	import '../app.css';
+	import 'lenis/dist/lenis.css';
 
 	let { children } = $props();
+
+	onMount(() => {
+		new Lenis({
+			autoRaf: true,
+			anchors: true
+		});
+	});
 </script>
 
 <Header />
