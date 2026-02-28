520b13260eb03f75a79ac0346a5bd662ea745c4b
vilas-gannarm <vilasgannarm@gmail.com>
2026-02-28 11:39:52 +0530

    feat: Implement initial portfolio and blog structure with new UI components, content, and assets.

---
+++ src/lib/components/portfolio/Navbar.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+	import { DATA } from '$lib/data/resume';
+	import Dock from '../magic/Dock.svelte';
+	import DockIcon from '../magic/DockIcon.svelte';
+	import * as Tooltip from '$lib/components/ui/tooltip';
+	import Separator from '$lib/components/ui/separator/separator.svelte';
+	import ModeToggle from './ModeToggle.svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { mode } from 'mode-watcher';
+	let theme = $derived(mode.current);
+</script>
+
+<div
+	class="pointer-events-none fixed inset-x-0 bottom-10 z-30 mx-auto mb-4 flex h-full max-h-14 origin-bottom"
+>
+	<div
+		class="bg-background dark:bg-background fixed inset-x-0 bottom-0 h-16 w-full to-transparent backdrop-blur-lg [-webkit-mask-image:linear-gradient(to_top,black,transparent)]"
+	></div>
+	<Dock
+		class="bg-background pointer-events-auto relative z-50 mx-auto flex h-full min-h-full transform-gpu items-center gap-0.5 rounded-full px-1 [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] sm:gap-1 md:gap-2 dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] dark:[border:1px_solid_rgba(255,255,255,.1)] "
+		let:magnification
+		let:distance
+		let:mouseX
+	>
+		{#each DATA.navbar as item (item.label)}
+			<DockIcon {magnification} {mouseX} {distance}>
+				<Tooltip.Root>
+					<Tooltip.Trigger>
+						<Button href={item.href} variant="ghost" size="icon" class="size-12 rounded-full">
+							<!-- <item.icon class="size-4" /> -->
+							<item.icon class="size-[18px]" strokeWidth={1.5} />
+						</Button>
+					</Tooltip.Trigger>
+					<Tooltip.Content>
+						<p>{item.label}</p>
+					</Tooltip.Content>
+				</Tooltip.Root>
+			</DockIcon>
+		{/each}
+		<Separator orientation="vertical" class="h-full" />
+		{#each Object.entries(DATA.contact.social)
+			.filter(([_, social]) => social.navbar)
+			.map(([_, social]) => social) as social (social.name)}
+			<DockIcon {magnification} {mouseX} {distance}>
+				<Tooltip.Root>
+					<Tooltip.Trigger>
+						<Button href={social.url} variant="ghost" size="icon" class="size-12 rounded-full">
+							<!-- <svelte:component this={social.icon} class="size-4" strokeWidth={1.5} /> -->
+							{#if social?.dark_icon && theme === 'dark'}
+								<img src={social?.dark_icon} class="size-4" alt={social.name} />
+							{:else}
+								<img src={social.icon} class="size-[18px]" alt={social.name} />
+							{/if}
+						</Button>
+					</Tooltip.Trigger>
+					<Tooltip.Content>
+						<p>{social.name}</p>
+					</Tooltip.Content>
+				</Tooltip.Root>
+			</DockIcon>
+		{/each}
+		<Separator orientation="vertical" class="h-full py-2" />
+		<DockIcon {magnification} {mouseX} {distance}>
+			<Tooltip.Root>
+				<Tooltip.Trigger>
+					<ModeToggle />
+				</Tooltip.Trigger>
+				<Tooltip.Content>
+					<p>Theme</p>
+				</Tooltip.Content>
+			</Tooltip.Root>
+		</DockIcon>
+	</Dock>
+</div>
---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import { formatDate } from '$lib/utils';
+	import BlurFade from '$lib/components/magic/BlurFade.svelte';
+
+	export let data;
+	let BLUR_FADE_DELAY = 0.04;
+</script>
+
+<svelte:head>
+	<title>Blog</title>
+</svelte:head>
+
+<BlurFade delay={BLUR_FADE_DELAY}>
+	<h1 class="mb-8 text-2xl font-medium tracking-tighter">Blog</h1>
+</BlurFade>
+<!-- Posts -->
+<section>
+	<ul class="posts">
+		{#each data.posts as post, id (id)}
+			<li class="post">
+				<BlurFade delay={BLUR_FADE_DELAY * 2 + id * 0.05}>
+					<a class="mb-4 flex flex-col space-y-1" href="/blog/{post.slug}">
+						<div class="flex w-full flex-col">
+							<p class="tracking-tight">{post.title}</p>
+							<p class="text-muted-foreground h-6 text-xs">
+								{formatDate(post.date)}
+							</p>
+						</div>
+					</a>
+				</BlurFade>
+			</li>
+		{/each}
+	</ul>
+</section>
+
+<style>
+	.posts {
+		display: grid;
+	}
+
+	.post {
+		max-inline-size: var(--size-content-3);
+	}
+
+	.post:not(:last-child) {
+		border-bottom: 1px solid var(--border);
+		padding-bottom: var(--size-7);
+	}
+</style>
