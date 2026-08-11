d1085d82f9d541496e7a47399324a9b80e77ddfc
vilas-gannaram <vilasgannaram@gmail.com>
2026-08-11 18:30:54 +0530

    feat: create Blog page with metadata and initial content

---
+++ app/blog/page.tsx
@@ -0,0 +1,14 @@
+export const metadata = {
+	title: 'Blog',
+	description: 'Read my blog.',
+};
+
+export default function Page() {
+	return (
+		<section>
+			<h1 className='font-semibold text-2xl mb-8 tracking-tighter'>My Blog</h1>
+
+			<p>Blogs/Notes adding soon ...</p>
+		</section>
+	);
+}
