cd2f62a7df70923b2c09099920f649d263c6d686
vilas-gannaram <vilasgannaram@gmail.com>
2026-08-26 22:59:33 +0530

    refactor: remove unnecessary classes from link in Home component

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-
 import { formatDate, getBlogPosts } from './get-posts';
 
 export const metadata = {
--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 					target='_blank'
 					rel='noopener noreferrer'
 					href='http://www.aganitha.ai'
-					className='underline underline-offset-2 text-neutral-600 dark:text-neutral-400'
+					className='underline underline-offset-2'
 				>
 					Aganitha Cognitive Solutions
 				</a>{' '}
